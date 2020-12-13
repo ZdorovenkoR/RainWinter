1e759781af6f2a1c33aea3ca9f1baa80b22292bc
ZdorovenkoR <75687097+ZdorovenkoR@users.noreply.github.com>
2020-12-13 16:06:46 +0600

    Update Program.cs

--- RainWinter/Program.cs
+++ RainWinter/Program.cs
@@ -10,6 +10,27 @@ namespace RainWinter
     {
         static void Main(string[] args)
         {
+            bool seathon;
+            bool temp;
+            Console.WriteLine("Введите номер месяца");
+            int month = Convert.ToInt32(Console.ReadLine());
+                if (month == 1|| month == 2|| month == 12)
+                seathon = true;
+                else
+                    return;
+            Console.WriteLine("Введите максимальную температуру");
+            int maxT = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите минимальную температуру");
+            int minT = Convert.ToInt32(Console.ReadLine());
+            int aveT = (maxT + minT)/2;
+            if (aveT > 0)
+                temp = true;
+            else
+                return;
+            if (seathon == true && temp == true)
+                Console.WriteLine("Дождливая зима");
+            else
+                return;
         }
     }
 }
